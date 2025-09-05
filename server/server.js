4d1c7a366803011c2fb7f949d77de96565429482
praffulsharma05 <praffulsharma38@gmail.com>
2025-09-05 21:28:59 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+// https://mern-auth-client-a4s6.onrender.com
+
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB();
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(express.json({ limit: "10mb" })); // ✅ Ensure JSON parsing
+app.use(express.urlencoded({ extended: true })); // ✅ Parse URL-encoded data
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+app.get("/", (req, res) => res.send("API Working"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+
+app.listen(port, () => console.log(`Server started on PORT: ${port}`));
