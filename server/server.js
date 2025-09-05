d035bf3029cecd0cb617f6d2c1415c23b83db734
prafful sharma <sharmaprafful646@gmail.com>
2025-09-05 22:01:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["mernauth-client.netlify.app"];
 
 app.use(express.json({ limit: "10mb" })); // ✅ Ensure JSON parsing
 app.use(express.urlencoded({ extended: true })); // ✅ Parse URL-encoded data
