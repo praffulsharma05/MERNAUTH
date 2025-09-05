586a1872e09e4efbbb12eaae1f525c59b65095b2
prafful sharma <sharmaprafful646@gmail.com>
2025-09-05 22:23:59 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ["mernauth-client.netlify.app"];
+const allowedOrigins = ["https://lucent-yeot-a29382.netlify.app"];
 
 app.use(express.json({ limit: "10mb" })); // ✅ Ensure JSON parsing
 app.use(express.urlencoded({ extended: true })); // ✅ Parse URL-encoded data
